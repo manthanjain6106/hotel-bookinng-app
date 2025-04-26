74050a8dc7ac678173398d07322f12373f24298c
manthanjain6106 <jainmanthan6106@gmail.com>
2025-04-26 18:31:09 +0530

    added new file

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import FeturedDestination from '../components/FeturedDestination'
 
 const Home = () => {
   return (
     <>
       <Hero/>
+      <FeturedDestination/>
     </>
   )
 }
